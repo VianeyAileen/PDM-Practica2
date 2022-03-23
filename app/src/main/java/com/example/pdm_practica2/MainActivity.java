6d9693f5c56290dd79b5af0acef034c708f6a389
VianeyAileen <vianeyborras@ciencias.unam.mx>
2022-03-22 22:06:42 -0600

    Agregando comentario

--- app/src/main/java/com/example/pdm_practica2/MainActivity.java
+++ app/src/main/java/com/example/pdm_practica2/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.pdm_practica2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/**
+ * @author Vianey Borrás "VianeyAileen" -
+ * <a href="mailto:vianeyborras@ciencias.unam.mx" >vianey@ciencias.unam.mx</a>
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
